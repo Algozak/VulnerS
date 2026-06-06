dfaf42fa6287dc818f2acb56cbfdee8ab1f83ec3
algozak <harutyunyanvazgen10@gmail.com>
2026-06-07 01:12:44 +0400

    feat:adding main() and error correction

--- auditor.sh
+++ auditor.sh
@@ -8,38 +8,43 @@ GREEN="\033[32m"
 
 # Search SUID files
 echo -e "${YELLOW} [*] Scanning for dangerous SUID binaries...${NC}"
-search-suid() {
+search_suid() {
   find / -type f -perm -4000 2> /dev/null | grep -v "/proc"
 }
-search_res=$(search-suid)
 
 # World-writable_files
 echo -e "${YELLOW} [*] Scanning for world-writable files...${NC}"
-world-writable() {
+world_writable() {
   find / -type f -perm -0002 2> /dev/null | grep -v "/proc"
 }
-world_writable_res=$(world-writable)
 
 # Check for empty password
 echo -e "${YELLOW} [*] Checing /etc/shadow for empty password...${NC}"
-empty-password() {
+empty_password() {
   awk -F: '$2 == "" { print $1 }' /etc/shadow
 }
-empty_password_res=$(empty-password)
 
 # Sudoers configuration
 sudoers() {
   sudo grep -r "NOPASSWD" /etc/sudoers /etc/sudoers.d/ 2> /dev/null | grep -v "^#"
 }
-sudoers_res=$(sudoers)
 
 # Check root
-check-root() {
+check_root() {
     if [ "$EUID" -ne 0 ]; then
         echo -e "${RED}[-] Error: This script must be run as root!${NC}"
         exit 1
     fi
 }
 check_root_res=$(check-root)
+  
+main() {
+  clear
+  check_root
+  search_suid
+  world_writable
+  empty_password
+  sudoers
+}
 
-
+main
