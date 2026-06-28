df7e0e02943b6c6dccfc7ad240dd319734ec7869
algozak <harutyunyanvazgen10@gmail.com>
2026-06-29 01:15:59 +0400

    Incorrect verification operation has been fixed - execstart_check.sh

--- checks/execstart_check.sh
+++ checks/execstart_check.sh
@@ -2,29 +2,27 @@
 
 
 #EcexStart check in units                                                                                  
-execstart() {                                                                               
-  echo ""                                                                                   
-  echo -e "${YELLOW} [*] Scanning for ExecStart in units...${NC}"                                
-  echo "--------------------------------------------------"                                 
-                                                                                            
-  local tmpfile=$(mktemp)                                                                   
-  (                                                                                          
-  grep -rh "^ExecStart=" /etc/systemd/system | while read -r line; do
+execstart() {
+  echo ""
+  echo -e "${YELLOW} [*] Scanning for ExecStart in units...${NC}"
+  echo "--------------------------------------------------"
+
+  local found_vuln=0
+
+  while read -r line; do
     cmd_payload=$(echo "$line" | cut -d= -f2-)
-    for word in "$cmd_payload"; do
-      if [[ "$word" == /* ]] && [ -f "$word" ]; then
-        if [ -w "$word" ]; then
-          echo -e "${RED}    [CRITICAL] Service use vulnerable file:(Availabe for writing!)${NC}" 
-          echo -e "\n    --- $word\n"
-          sleep 1  
-        else
-          echo -e "${GREEN}    [OK] No vulnerabilities found."
-          sleep 1  
-        fi
+    for word in $cmd_payload; do
+      if [[ "$word" == /* ]] && [ -f "$word" ] && [ -w "$word" ]; then
+        echo -e "${RED}    [CRITICAL] Service uses vulnerable file (writable!):${NC}"
+        echo -e "\n    --- $word\n"
+        found_vuln=1
       fi
     done
-  done 
-  ) 
+  done < <(grep -rh "^ExecStart=" /etc/systemd/system)
+
+  if [ "$found_vuln" -eq 0 ]; then
+    echo -e "${GREEN}    [OK] No vulnerabilities found.\n${NC}"
+  fi
 }
 
 
