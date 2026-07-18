6adad672a5e3d40031e86cef046b8d6d6b5ad2c8
algozak <harutyunyanvazgen10@gmail.com>
2026-07-18 16:30:20 +0400

    A whitelist is required for those files that are allowed to have SUID

--- checks/suid.sh
+++ checks/suid.sh
@@ -7,7 +7,44 @@ search_suid() {
   echo ""                                                                 
   echo -e "${YELLOW} [*] Scanning for dangerous SUID binaries...${NC}"    
   echo "--------------------------------------------------"               
-                                                                          
+   
+  local whitelist=(
+    "/usr/bin/at"
+    "/usr/bin/crontab"
+    "/usr/bin/fusermount"
+    "/usr/bin/fusermount3"
+    "/usr/bin/pam_timestamp_check"
+    "/usr/bin/pkexec"
+    "/usr/bin/unix_chkpwd"
+    "/usr/bin/userhelper"
+    "/usr/bin/chage"
+    "/usr/bin/gpasswd"
+    "/usr/bin/newgrp"
+    "/usr/bin/passwd"
+    "/usr/bin/sudo"
+    "/usr/bin/vmware-user-suid-wrapper"
+    "/usr/bin/grub2-set-bootflag"
+    "/usr/bin/fusermount-glusterfs"
+    "/usr/bin/mount.nfs"
+    "/usr/bin/mount"
+    "/usr/bin/umount"
+    "/usr/bin/chfn"
+    "/usr/bin/chsh"
+    "/usr/bin/su"
+    "/usr/lib/polkit-1/polkit-agent-helper-1"
+    "/usr/lib/virtualbox/VBoxHeadless"
+    "/usr/lib/virtualbox/VBoxNetAdpCtl"
+    "/usr/lib/virtualbox/VBoxNetDHCP"
+    "/usr/lib/virtualbox/VBoxNetNAT"
+    "/usr/lib/virtualbox/VBoxVolInfo"
+    "/usr/lib/virtualbox/VirtualBoxVM"
+    "/usr/lib64/cef/chrome-sandbox"
+    "/usr/libexec/dbus-1/dbus-daemon-launch-helper"
+    "/usr/libexec/spice-gtk-x86_64/spice-client-glib-usb-acl-helper"
+    "/usr/libexec/libgtop_server2"
+    "/usr/libexec/qemu-bridge-helper"
+  )
+
   local tmpfile                                                                        
   local suid_var 
   tmpfile=$(mktemp)
@@ -16,7 +53,7 @@ search_suid() {
                                                                           
   spinner $!               
 
-  suid_var=$(cat "$tmpfile")
+  suid_var=$(comm -23 <(sort "$tmpfile") <(printf '%s\n' "${whitelist[@]}" | sort))
   rm -f "$tmpfile"
                                                                         
   if [ -z "$suid_var" ]; then                                             
